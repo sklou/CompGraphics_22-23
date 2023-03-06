f4e1312d2d8f826752e9f71ca369c5db13e9d533
NeteroDano <72193562+NeteroDano@users.noreply.github.com>
2023-03-06 22:27:05 +0200

    Adding Intersects to Sphere

--- Basic_RayCaster/Structures/Sphere.cs
+++ Basic_RayCaster/Structures/Sphere.cs
@@ -17,9 +17,20 @@ namespace Basic_RayCaster.Structures
             this.radius = radius;
         }
 
-        /*public bool Intersect(Ray ray)
+        public bool Intersects(Ray ray)
         {
-           
-        }*/
+            Vector rayOriginToSphere = position - ray.origin;
+            float closest_point = Vector.Dot(rayOriginToSphere, ray.direction);
+            if (closest_point < 0)
+            {
+                return false;
+            }
+            float d2 = Vector.Dot(rayOriginToSphere, rayOriginToSphere) - closest_point * closest_point;
+            if (d2 > radius * radius)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
