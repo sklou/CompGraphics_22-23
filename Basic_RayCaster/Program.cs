f1a520af051bb5294335c9a44994c6946b6f3127
sklou <sssokolov.d@ukr.net>
2023-03-07 00:10:32 +0200

    GetRayThroughPixel

--- Basic_RayCaster/Program.cs
+++ Basic_RayCaster/Program.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using Basic_RayCaster.Structures;
+﻿using Basic_RayCaster.Structures;
+using System;
+using Test;
 
 
 class Program
 {
     static void Main(string[] args)
     {
-      
+        Sphere sphere = new Sphere(new Vector(0f, 0f, 10f), 4f);
+        Camera camera = new Camera(new Vector(0f, 0f, 0f));
+
+        int width = 120;
+        int height = 80;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Ray ray = camera.GetRayThroughPixel(x, y, width, height);
+                if (sphere.Intersects(ray))
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
     }
 }
--- Basic_RayCaster/Structures/Camera.cs
+++ Basic_RayCaster/Structures/Camera.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Basic_RayCaster.Structures
+namespace Test
 {
     struct Camera
     {
@@ -16,9 +16,18 @@ namespace Basic_RayCaster.Structures
             this.position = position;
         }
 
-       /* public Ray GetRay_For_Pixel(int x, int y, int width, int height)
+        public Ray GetRayThroughPixel(int x, int y, int width, int height)
         {
-           
-        }*/
+            float aspectRatio = width / height;
+            float fov = 90f * (float)Math.PI / 180f; //Поле зору(fov) камери встановлюється на 60 градусів і перетворюється в радіани
+            float halfHeight = (float)Math.Tan(fov / 2f);
+            float halfWidth = aspectRatio * halfHeight;
+            float pixelWidth = halfWidth * 2f / width;
+            float pixelHeight = halfHeight * 2f / height;
+
+            Vector direction = new Vector(x * pixelWidth - halfWidth, y * pixelHeight - halfHeight, 1f);
+            direction.Normalize();
+            return new Ray(position, direction);
+        }
     }
 }
