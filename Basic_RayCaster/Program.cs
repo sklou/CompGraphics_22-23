ad4e318357bf699c28929c4c5bf163532058f415
mcmcmax437 <mcmcmax437@gmail.com>
2023-03-08 23:03:49 +0200

    Add light to the sphere
    (second try, problem with branches)

--- Basic_RayCaster/Program.cs
+++ Basic_RayCaster/Program.cs
@@ -12,8 +12,12 @@ class Program
         Plane plane = new Plane(new Vector(0f, 0f, 10f), new Vector(0f, 0f, 1f));
         Disc disc = new Disc(new Vector(0f, 0f, 10f), 6f, 3f);
         // Disc disc = new Disc(new Vector(0f, 0f, 10f), new Vector(0, 0, 1), 3f);
-        Sphere sphere = new Sphere(new Vector(0f, 0f, 10f), 2f);
+        Sphere sphere = new Sphere(new Vector(0f, 0f, 8f), 4f);
         Camera camera = new Camera(new Vector(0f, 0f, 0f));
+        Vector L = new Vector(3, 2, 1);
+        L.Normalize();
+
+
 
         int width = 120;
         int height = 60;
@@ -23,22 +27,63 @@ class Program
             for (int x = 0; x < width; x++)
             {
                 Ray ray = camera.GetRayThroughPixel(x, y, width, height);
-                //if (plane.Intersects(ray))
-                //if (disc.Intersects(ray))
-                //if (sphere.Intersects(ray))
-                if (sphere.Intersects(ray).intersection != null || sphere.Intersects(ray).normal != null)
+                float result;
+
+                if (sphere.Intersects(ray).normal.HasValue)
                 {
-                    Console.Write("#");
+                    result = Vector.Dot(sphere.Intersects(ray).normal, L);
+
+
+                    if (result < 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (result < 0.2)
+                    {
+                        Console.Write(".");
+                    }
+                    else if (result < 0.5)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (result < 0.8)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
                 }
                 else
                 {
                     Console.Write(" ");
+
                 }
+                //if (plane.Intersects(ray))
+                //if (disc.Intersects(ray))
+                //if (sphere.Intersects(ray))
+                /* if (sphere.Intersects(ray).intersection != null || sphere.Intersects(ray).normal != null)
+                 {
+                     Console.Write("#");
+                 }
+                 else
+                 {
+                     Console.Write(" ");
+                 }
+                */
+
+
+
+                // Vector normal = new Vector(sphere.Intersects(ray).normal);
+
+
+
             }
             Console.WriteLine();
         }
-    
-        
+
+
         SphereIntersectionTests intersectionTests = new SphereIntersectionTests();
         intersectionTests.SphereIntersectionTrue();
         intersectionTests.SphereIntersectionFalse();
--- Basic_RayCaster/Structures/Vector.cs
+++ Basic_RayCaster/Structures/Vector.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,8 +18,23 @@ namespace Basic_RayCaster.Structures
             this.y = y;
             this.z = z;
         }
+        public static Vector Zero
+        {
+            get { return new Vector(0.0f, 0.0f, 0.0f); }
+        }
 
-        
+        public static Vector ToVector(Vector? nullableVector)
+        {
+            if (nullableVector == null)
+            {
+                return Vector.Zero;
+            }
+            else
+            {
+                return nullableVector.Value;
+            }
+        }
+
         public static Vector operator +(Vector a, Vector b)
         {
             return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
@@ -39,6 +53,12 @@ namespace Basic_RayCaster.Structures
         {
             return a.x * b.x + a.y * b.y + a.z * b.z;
         }
+
+        public static float Dot2(Vector? a, Vector b)
+        {
+            a.Value.Normalize();
+            return a.Value.x * b.x + a.Value.y * b.y + a.Value.z * b.z;
+        }
         public static float Distance(Vector a, Vector b)
         {
             return (float)Math.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
@@ -53,6 +73,25 @@ namespace Basic_RayCaster.Structures
             x /= magn;
             y /= magn;
             z /= magn;
+        }
+
+        public static float Dot(Vector? normal, Vector l)
+        {
+            normal.Value.Normalize();
+            Vector value = normal.Value;
+            value.x = -1 * normal.Value.x;
+            value.y = -1 * normal.Value.y;
+            value.z = -1 * normal.Value.z;
+
+
+            Vector n = (Vector)value;
+            // Vector n = (Vector)normal;
+            l.Normalize();
+            Vector v = l;
+
+            float dotProduct = n.x * v.x + n.y * v.y + n.z * v.z;
+
+            return dotProduct;
         }
     }
 }
