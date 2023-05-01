0ee29c4531130c523141a89b338e644204acde42
mcmcmax437 <mcmcmax437@gmail.com>
2023-05-01 15:45:33 +0300

    Laba2 - Launch with console, convert ppm to bmp
    basic, without any dll, tried to make it - don`t want to work, will think about it later

--- Convertor/Program.cs
+++ Convertor/Program.cs
@@ -1,84 +1,198 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
-namespace PpmToBmpConverter
+
+//Program.exe --source=D:\GitLab\CompGraphics_22-23\Convertor\Test_pic\test.ppm --goal-format=bmp
+class Program
 {
-    public interface IPpmInput
+    static void Main(string[] args)
     {
-        byte[] GetPpmData();
-    }
-
-    public class FilePpmInput : IPpmInput
-    {
-        private readonly string _fileName;
-
-        public FilePpmInput(string fileName)
+        string sourcePath = null;
+        string goalFormat = null;
+        string outputPath = null;
+        for (int i = 0; i < args.Length; i++)
         {
-            _fileName = fileName;
-        }
-
-        public byte[] GetPpmData()
-        {
-            return File.ReadAllBytes(_fileName);
-        }
-    }
-
-    public class PpmImage
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public byte[] Pixels { get; set; }
-
-        public PpmImage(byte[] data)
-        {
-            var header = Encoding.ASCII.GetString(data.TakeWhile(b => b != 10).ToArray());
-            var dimensions = header.Split(' ').Skip(1).Select(int.Parse).ToArray();
-            Width = 640;
-            Height = 426;
-            var pixelData = data.Skip(header.Length + 1).ToArray();
-            Pixels = pixelData;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var ppmInput = new FilePpmInput("D:\\GitLab\\CompGraphics_22-23\\Convertor\\Test_pic\\test_ppm.ppm");
-            var ppmData = ppmInput.GetPpmData();
-            var bmpData = ConvertPpmToBmp(ppmData);
-            File.WriteAllBytes("D:\\GitLab\\CompGraphics_22-23\\Convertor\\Test_pic\\output.bmp", bmpData);
-        }
-
-        public static byte[] ConvertPpmToBmp(byte[] ppmData)
-        {
-            var image = new PpmImage(ppmData);
-
-            var bmpData = new List<byte>();
-            bmpData.AddRange(Encoding.ASCII.GetBytes("BM"));
-            bmpData.AddRange(BitConverter.GetBytes(54 + image.Pixels.Length));
-            bmpData.AddRange(new byte[] { 0, 0, 0, 0 });
-            bmpData.AddRange(BitConverter.GetBytes(54));
-            bmpData.AddRange(BitConverter.GetBytes(40));
-            bmpData.AddRange(BitConverter.GetBytes(image.Width));
-            bmpData.AddRange(BitConverter.GetBytes(image.Height));
-            bmpData.AddRange(new byte[] { 1, 0 });
-            bmpData.AddRange(new byte[] { 24, 0 });
-            bmpData.AddRange(new byte[] { 0, 0, 0, 0 });
-            bmpData.AddRange(BitConverter.GetBytes(image.Pixels.Length));
-            bmpData.AddRange(new byte[] { 0, 0, 0, 0 });
-
-            for (int i = 0; i < image.Pixels.Length; i += 3)
+            if (args[i].StartsWith("--source="))
             {
-                bmpData.Add(image.Pixels[i + 2]); // Blue
-                bmpData.Add(image.Pixels[i + 1]); // Green
-                bmpData.Add(image.Pixels[i]);     // Red
+                sourcePath = args[i].Substring("--source=".Length);
             }
-
-            return bmpData.ToArray();
+            else if (args[i].StartsWith("--goal-format="))
+            {
+                goalFormat = args[i].Substring("--goal-format=".Length);
+            }
+            else if (args[i].StartsWith("--output="))
+            {
+                outputPath = args[i].Substring("--output=".Length);
+            }
+            else
+            {
+                Console.Error.WriteLine($"Invalid argument: {args[i]}");
+                return;
+            }
         }
+        if (sourcePath == null)
+        {
+            Console.Error.WriteLine("Missing --source argument");
+            return;
+        }
+        if (goalFormat == null)
+        {
+            Console.Error.WriteLine("Missing --goal-format argument");
+            return;
+        }
+        if (!goalFormat.Equals("bmp", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.Error.WriteLine($"Invalid goal format: {goalFormat}");
+            return;
+        }
+        if (outputPath == null)
+        {
+            outputPath = Path.ChangeExtension(sourcePath, ".bmp");
+        }
+
+       
+        byte[] ppmData;
+        int ppmWidth;
+        int ppmHeight;
+        try
+        {
+            using (FileStream ppmFile = File.OpenRead(sourcePath))
+            {
+                using (BinaryReader ppmReader = new BinaryReader(ppmFile))
+                {
+                    string ppmMagic = Encoding.ASCII.GetString(ppmReader.ReadBytes(2));
+                    if (!ppmMagic.Equals("P6"))
+                    {
+                        Console.Error.WriteLine("Invalid PPM magic number");
+                        return;
+                    }
+                    int ppmMaxValue = 0;
+                    while (true)
+                    {
+                        char c = (char)ppmReader.ReadByte();
+                        if (c == '#')
+                        {
+                            while (ppmReader.ReadByte() != '\n') ;
+                        }
+                        else if (char.IsDigit(c))
+                        {
+                            ppmMaxValue = ppmMaxValue * 10 + (c - '0');
+                        }
+                        else if (c == '\n')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid PPM header");
+                            return;
+                        }
+                    }
+                    ppmWidth = 0;
+                    ppmHeight = 0;
+                    while (true)
+                    {
+                        char c = (char)ppmReader.ReadByte();
+                        if (c == '#')
+                        {
+                            while (ppmReader.ReadByte() != '\n') ;
+                        }
+                        else if (char.IsDigit(c))
+                        {
+                            ppmWidth = ppmWidth * 10 + (c - '0');
+                        }
+                        else if (c == ' ')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid PPM header");
+                            return;
+                        }
+                    }
+                    while (true)
+                    {
+                        char c = (char)ppmReader.ReadByte();
+                        if (c == '#')
+                        {
+                            while (ppmReader.ReadByte() != '\n') ;
+                        }
+                        else if (char.IsDigit(c))
+                        {
+                            ppmHeight = ppmHeight * 10 + (c - '0');
+                        }
+                        else if (c == '\n')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid PPM header");
+                            return;
+                        }
+                    }
+                    int ppmDataSize = ppmWidth * ppmHeight * 3;
+                    ppmData = new byte[ppmDataSize];
+                    int bytesRead = ppmReader.Read(ppmData, 0, ppmDataSize);
+                    if (bytesRead != ppmDataSize)
+                    {
+                        Console.Error.WriteLine("Invalid PPM data size");
+                        return;
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"Error reading PPM file: {e.Message}");
+            return;
+        }
+
+        
+        byte[] bmpData = new byte[ppmWidth * ppmHeight * 3];
+        for (int y = 0; y < ppmHeight; y++)
+        {
+            for (int x = 0; x < ppmWidth; x++)
+            {
+                int ppmOffset = (y * ppmWidth + x) * 3;
+                int bmpOffset1 = ((ppmHeight - y - 1) * ppmWidth + x) * 3;
+                bmpData[bmpOffset1 + 2] = ppmData[ppmOffset+1];     // Blue
+                bmpData[bmpOffset1] = ppmData[ppmOffset]; // Green
+                bmpData[bmpOffset1+1] = ppmData[ppmOffset + 2];     // Red
+            }
+        }
+        int bmpSize = 14 + 40 + bmpData.Length;
+        int bmpOffset = 14 + 40;
+        byte[] bmpHeader = new byte[] {
+            0x42, 0x4D,                       
+        (byte)(bmpSize), (byte)(bmpSize >> 8), (byte)(bmpSize >> 16), (byte)(bmpSize >> 24), 
+            0, 0, 0, 0,                       
+        (byte)(bmpOffset), (byte)(bmpOffset >> 8), (byte)(bmpOffset >> 16), (byte)(bmpOffset >> 24), 
+            40, 0, 0, 0,                     
+        (byte)(ppmWidth), (byte)(ppmWidth >> 8), (byte)(ppmWidth >> 16), (byte)(ppmWidth >> 24), // Width
+        (byte)(ppmHeight), (byte)(ppmHeight >> 8), (byte)(ppmHeight >> 16), (byte)(ppmHeight >> 24), // Height
+            1, 0, // Color Planes
+            24, 0, // Bits per Pixel
+            0, 0, 0, 0, // Compression
+        (byte)(bmpData.Length), (byte)(bmpData.Length >> 8), (byte)(bmpData.Length >> 16), (byte)(bmpData.Length >> 24), // Image Size
+            0, 0, 0, 0, // X Pixels per Meter
+            0, 0, 0, 0, // Y Pixels per Meter
+            0, 0, 0, 0, // Colors Used
+            0, 0, 0, 0, // Colors Important
+        };
+
+
+
+        using (FileStream bmpStream = new FileStream(outputPath, FileMode.Create))
+        {
+            bmpStream.Write(bmpHeader, 0, bmpHeader.Length);
+            bmpStream.Write(bmpData, 0, bmpData.Length);
+        }
+        Console.WriteLine($"Conversion complete: {outputPath}");
     }
 }
+
+
+
